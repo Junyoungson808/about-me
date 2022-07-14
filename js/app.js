bfcc46cd757518fe8cc452e8b0c82a7d16748030
Junyoung Son <junyoungson808@gmail.com>
2022-07-14 13:57:37 -0700

    Fixed CSS, added q6 & q7 in app.js.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let score = 0;
+
 let userName = prompt('What is your name?');
   console.log(`Hello ${userName}, welcome to my site!`);
   alert(`Welcome to my site ${userName}, please answer these questions!`);
@@ -8,6 +10,7 @@ let qOne = prompt('Do I like to travel?').toLowerCase();
 
 if(qOne === 'y' || qOne === 'yes'){
   alert('You are right! ' + userName);
+  score++;
 } else if(qOne === 'n' || qOne === 'no'){
   alert('You are wrong');
 } else{
@@ -18,6 +21,7 @@ let qTwo = prompt('Did I serve in the Military?');
 
 if(qTwo === 'y' || qTwo === 'yes'){
   alert('You are right!');
+  score++;
 } else if(qTwo === 'n' || qTwo === 'no'){
   alert('You are wrong');
 } else{
@@ -28,6 +32,7 @@ let qThree = prompt('Am I big into model kits?');
 
 if(qThree === 'y' || qThree === 'yes'){
   alert('You are right! I enjoy model kits preferably of robots and cars!');
+  score++;
 } else if(qThree === 'n' || qThree === 'no'){
   alert('You are wrong');
 } else{
@@ -40,6 +45,7 @@ if(qFour === 'y' || qFour === 'yes'){
   alert('You are very wrong!!!');
 } else if(qFour === 'n' || qFour === 'no'){
   alert('You are very very right!!!');
+  score++;
 } else{
   alert('Answer yes or no');
 }
@@ -48,11 +54,13 @@ let qFive = prompt('Have I ever lived in New Zealand?');
 
 if(qFive === 'y' || qFive === 'yes'){
   alert('You are right!');
+  score++;
 } else if(qFive === 'n' || qFive === 'no'){
   alert('Wrong... I lived in New Zealand!');
 } else{
   alert('Answer yes or no');
 }
+
 
 let clueGuesses = 5;
 let answer = 6;
@@ -66,6 +74,7 @@ while(clueGuesses && !rightanswer){
   if(parseInt(userGuess) === answer){
     rightanswer = true;
     alert('good job');
+    score++;
   } else if(userGuess < answer) {
     alert(`You are too low, Guesses Remaining: ${clueGuesses}`)}
   else if(userGuess > answer) {
@@ -74,5 +83,23 @@ while(clueGuesses && !rightanswer){
     alert(`it was ${answer}, Guesses Remaining: ${clueGuesses}`);
   }
 }
+
+let favEdmArt = ['illenium', 'slander', 'excision', 'rezz', 'porter robinson', '3lau'];
+
+for(let i = 0; i < 6; i++){
+  let guessMyFav = prompt('Guess my favorite edm artists!').toLowerCase();
+
+  for(let j = 0; j < guessMyFav.length; j++){
+    if(guessMyFav === favEdmArt[j]){
+      alert('Good stuff!');
+      score++;
+      i = 6;
+      break;
+    }
+  }
+}
+alert(`These are the possible quesses: ${favEdmArt}`);
+alert(`You scored ${score}`);
+
 
 let goodbye = prompt(`Thank you ${userName} for visiting my site! Hope you enjoyed your stay! see you again soon.`);
